3501cf4f5dbbf565aed120b156f6e81fe500a5dc
Denis Domscheit <denis.domscheit@gmail.com>
2025-12-29 14:05:20 +0100

    Change filese for dockermountible

--- quartz/cli/args.js
+++ quartz/cli/args.js
@@ -63,7 +63,7 @@ export const BuildArgv = {
   output: {
     string: true,
     alias: ["o"],
-    default: "public",
+    default: "public/www",
     describe: "output folder for files",
   },
   serve: {
